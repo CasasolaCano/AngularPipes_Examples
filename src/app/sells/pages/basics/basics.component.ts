1d75640270437bd746add4b872da32f36c1c7af8
CasasolaCano <casasolacontact@gmail.com>
2021-05-23 23:45:46 +0200

    lowercase, uppercase, titlecase pipes

--- src/app/sells/pages/basics/basics.component.ts
+++ src/app/sells/pages/basics/basics.component.ts
@@ -1,15 +1,14 @@
-import { Component, OnInit } from '@angular/core';
+import { Component} from '@angular/core';
 
 @Component({
   selector: 'app-basics',
   templateUrl: './basics.component.html',
   styleUrls: ['./basics.component.css']
 })
-export class BasicsComponent implements OnInit {
+export class BasicsComponent  {
 
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  nameLower:string = "FRANCISCO"
+  nameUpper:string = "francisco"
+  nameComplete:string = "fRanCiSco"
 
 }
