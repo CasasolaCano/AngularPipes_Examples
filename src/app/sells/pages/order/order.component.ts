adaa59892e64a28c072111f3594707dd232026a1
CasasolaCano <casasolacontact@gmail.com>
2021-06-08 14:02:17 +0200

    Adding info to the PrimeTable

--- src/app/sells/pages/order/order.component.ts
+++ src/app/sells/pages/order/order.component.ts
@@ -1,17 +1,40 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
+import { Color, Heroes } from '../../interfaces/sells.interfaces';
 
 @Component({
   selector: 'app-order',
   templateUrl: './order.component.html',
   styleUrls: ['./order.component.css']
 })
-export class OrderComponent implements OnInit {
+export class OrderComponent {
 
   mayus:boolean = false;
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
+  heroes:Heroes[] = [
+    {
+      name:'Superman',
+      vuela:true,
+      color: Color.azul
+    },
+    {
+      name:'Batman',
+      vuela:false,
+      color: Color.negro
+    },
+    {
+      name:'Robin',
+      vuela:false,
+      color: Color.verde
+    },
+    {
+      name:'Daredevil',
+      vuela:false,
+      color: Color.rojo
+    },
+    {
+      name:'Linterna Verde',
+      vuela:true,
+      color: Color.verde
+    }
+  ]
 
 }
